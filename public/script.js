b82994e9f3916c208393a72cb7cb31c233b31860
Anthony Vladimir Gotay <39626593+VladlmirG@users.noreply.github.com>
2023-11-24 14:32:15 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,50 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const video = document.getElementById('videoPlayer');
-    const playPauseBtn = document.getElementById('playPauseBtn');
-    const volumeControl = document.getElementById('volumeControl');
-    const customControls = document.getElementById('custom-controls');
-    const liveDot = document.getElementById('liveDot');
     
-    let isPlaying = false; // Track the playback state
-
     // WebSocket connection
     const ws = new WebSocket('ws://localhost:3000');
 
-    ws.onmessage = async function(event) {
+    ws.onmessage = function(event) {
         const data = JSON.parse(event.data);
-        await processWebSocketMessage(data);
+        processWebSocketMessage(data);
     };
 
-    async function processWebSocketMessage(data) {
+    function processWebSocketMessage(data) {
         if (data.type === 'currentTime') {
             // Update the video's currentTime without triggering a seek event
             video.currentTime = data.data;
         }
     }
 
-    playPauseBtn.addEventListener('click', function() {
-        if (isPlaying) {
-            video.pause();
-            playPauseBtn.textContent = 'Play';
-        } else {
-            video.play();
-            playPauseBtn.textContent = 'Pause';
-        }
-        isPlaying = !isPlaying;
-        sendUpdateTime();
+    video.addEventListener('loadeddata', function() {
+        // Start the loop when the video is loaded
+        video.play();
     });
-
-    video.addEventListener('timeupdate', function() {
-        sendUpdateTime();
-    });
-
-    volumeControl.addEventListener('input', function() {
-        video.volume = volumeControl.value;
-    });
-
-
-    async function sendUpdateTime() {
-        const currentTime = video.currentTime;
-        ws.send(JSON.stringify({ type: 'updateTime', data: currentTime }));
-    }
 });
+
