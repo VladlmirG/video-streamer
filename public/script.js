4b463751faaad3fd3deaee371513b43f6d1f00db
Anthony Vladimir Gotay <39626593+VladlmirG@users.noreply.github.com>
2023-11-24 14:14:36 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,46 +1,62 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const video = document.getElementById('videoPlayer');
-    const playPauseBtn = document.getElementById('playPauseBtn');
-    const volumeControl = document.getElementById('volumeControl');
-    const customControls = document.getElementById('custom-controls');
-    const liveDot = document.getElementById('liveDot');
-    
-    let isPlaying = false; // Track the playback state
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
 
-    // WebSocket connection
-    const ws = new WebSocket('ws://localhost:3000');
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-    ws.onmessage = async function(event) {
-        const data = JSON.parse(event.data);
-        await processWebSocketMessage(data);
-    };
+let currentPlaybackTime = 0;
 
-    async function processWebSocketMessage(data) {
-        if (data.type === 'currentTime') {
-            // Update the video's currentTime without triggering a seek event
-            video.currentTime = data.data;
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set proper headers to allow cross-origin requests
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+// Serve video files from the 'videos' directory
+app.use('/videos', express.static(path.join(__dirname, 'public', 'videos')));
+
+// WebSocket connection handling
+wss.on('connection', (ws) => {
+    // Send the current playback time to the new client
+    ws.send(JSON.stringify({ type: 'currentTime', data: currentPlaybackTime }));
+
+    // Handle messages from clients
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+        if (data.type === 'updateTime') {
+            // Update the current playback time
+            currentPlaybackTime = data.data;
+            // Broadcast the updated time to all connected clients
+            broadcastUpdateTime();
         }
-    }
-    
-    playPauseBtn.addEventListener('click', function() {
-        if (isPlaying) {
-            video.pause();
-            playPauseBtn.textContent = 'Play';
-        } else {
-            video.play();
-            playPauseBtn.textContent = 'Pause';
+    });
+
+    // Handle WebSocket connection closing
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+function broadcastUpdateTime() {
+    // Broadcast the updated time to all connected clients
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'currentTime', data: currentPlaybackTime }));
         }
-        isPlaying = !isPlaying;
-        sendUpdateTime();
     });
+}
 
-    video.addEventListener('timeupdate', function() {
-        sendUpdateTime();
-    });
-
-    volumeControl.addEventListener('input', function() {
-        video.volume = volumeControl.value;
-    });
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
 
     async function sendUpdateTime() {
