848f29f559a832575078c74058f3201665feca17
Anthony Vladimir Gotay <39626593+VladlmirG@users.noreply.github.com>
2023-11-24 22:37:57 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ server, maxPayload: 1024 * 1024 * 10 }); // 10 MB
 
 let currentPlaybackTime = 0;
+let clients = new Set();
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -27,26 +28,37 @@ wss.on('connection', (ws) => {
     // Send the current playback time to the new client
     ws.send(JSON.stringify({ type: 'currentTime', data: currentPlaybackTime }));
 
+    // Add the new client to the set
+    clients.add(ws);
+
     // Handle messages from clients
     ws.on('message', (message) => {
         const data = JSON.parse(message);
         if (data.type === 'updateTime') {
             // Update the current playback time
             currentPlaybackTime = data.data;
+
             // Broadcast the updated time to all connected clients
-            wss.clients.forEach((client) => {
-                if (client !== ws && client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({ type: 'currentTime', data: currentPlaybackTime }));
-                }
-            });
+            broadcast({ type: 'currentTime', data: currentPlaybackTime });
         }
     });
 
     // Handle WebSocket connection closing
     ws.on('close', () => {
         console.log('Client disconnected');
+        // Remove the disconnected client from the set
+        clients.delete(ws);
     });
 });
+
+function broadcast(data) {
+    const message = JSON.stringify(data);
+    clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    });
+}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
